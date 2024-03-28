b906bddb5e8f026c79ef4863d1ee686608e232ba
Vansh Chawla <vansh.chawla.ug21@nsut.ac.in>
2024-03-28 20:26:20 +0530

    upd 2 : Updated Tests

--- tests/step-04/index.test.js
+++ tests/step-04/index.test.js
@@ -15,7 +15,7 @@ test('Parse SQL Query', () => {
     const parsed = parseQuery(query);
     expect(parsed).toEqual({
         fields: ['id', 'name'],
-        whereClauses: Array [],
+        whereClauses: [],
         joinCondition: null,
         joinTable: null,
         table: 'sample'
